57b12afac8a2042daa4badf8b747dd7910f071f5
pavlo <nadazasada@gmail.com>
2026-05-15 14:36:03 +0300

    make build

---
+++ index.5d74ffc5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.position="relative",t.style.backgroundColor="red",e.style.display="flex",e.style.alignItems="center",e.style.justifyContent="center";
+//# sourceMappingURL=index.5d74ffc5.js.map
