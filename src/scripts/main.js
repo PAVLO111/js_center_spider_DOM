9c3e7d25f0a2482cbeee171b52baf6c091e34f57
pavlo <nadazasada@gmail.com>
2026-05-15 14:31:23 +0300

    inplementation flexbox

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,17 @@
 'use strict';
 
 const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+
+// -1-
+// spider.style.top = '45%'
+// spider.style.left = '45%'
+
+// -2-
+spider.style.position = 'relative';
+spider.style.backgroundColor = 'red';
+
 wall.style.display = 'flex';
-wall.style.alignItem = 'center';
+wall.style.alignItems = 'center';
 wall.style.justifyContent = 'center';
