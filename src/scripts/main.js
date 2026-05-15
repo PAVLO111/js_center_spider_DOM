4aa61bdc1d0d075d6f3fb233afc69ef43248b256
pavlo <nadazasada@gmail.com>
2026-05-15 17:16:58 +0300

    add position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,5 +9,6 @@ const wallWidth = wall.clientWidth;
 const spiderHeight = spider.clientHeight;
 const spiderWidth = spider.clientWidth;
 
+spider.style.position = 'absolute';
 spider.style.top = (wallHeight - spiderHeight) / 2 + 'px';
 spider.style.left = (wallWidth - spiderWidth) / 2 + 'px';
